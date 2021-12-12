3edee467a1a767bc0918e6ccbd3037cf29c113fe
Nursurtan <arapovnur23@gmail.com>
2021-12-12 20:15:59 +0700

    Initialised the First Question

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let allQuestions = QuestionBank()
+    var pickedAnswer : Bool = false
     //Place your instance variables here
     
     
@@ -21,10 +23,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
   
     }
     
@@ -41,6 +53,17 @@ class ViewController: UIViewController {
     
     func checkAnswer() {
         
+        let correctAnswer = allQuestions.list[0].answer
+        
+        if correctAnswer == pickedAnswer {
+            print("You got it!")
+        }
+        else {
+            print("wrong!")
+        }
+    
+      
+    
     }
     
     
